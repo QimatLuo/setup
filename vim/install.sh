945fe9ffbc1cd25d7e74cddc60694da55e0564c7
Qimat Luo <qimatluo@exosite.com>
2018-02-17 08:21:14 +0800

    Bug fix

--- vim/install.sh
+++ vim/install.sh
@@ -13,12 +13,6 @@ if [[ -f $file ]]; then
 fi
 ln -s ".editorconfig" $file
 
-file="$HOME/.vim/bundle/vim-snippets/snippets/javascript/javascript.snippets"
-if [[ -f $file ]]; then
-	rm $file
-fi
-ln -s "$path/javascript.snippets" $file
-
 file="$HOME/.vim/.jshintrc"
 if [[ -f $file ]]; then
 	rm $file
@@ -30,3 +24,9 @@ if [[ ! -f $file ]]; then
   git clone https://github.com/gmarik/Vundle.vim.git $file
 fi
 vim +PluginInstall +qall
+
+file="$HOME/.vim/bundle/vim-snippets/snippets/javascript/javascript.snippets"
+if [[ -f $file ]]; then
+	rm $file
+fi
+ln -s "$path/javascript.snippets" $file
