7a13823eabe5217663ac15a652996ba33cd3f73c
Qimat Luo <qimatluo@exosite.com>
2018-04-23 16:15:47 +0800

    Add github pull request shell

---
+++ bash/pr.sh
@@ -0,0 +1,9 @@
+user=`whoami`
+owner=`git remote -v | head -1 | cut -f2 | cut -d'/' -f1 | cut -d':' -f2`
+repo=`git remote -v | head -1 | cut -f2 | cut -d'/' -f2  | cut -d'.' -f1`
+base=$1
+head=`git rev-parse --abbrev-ref HEAD`
+pr=`curl -X POST -u $user:$GITHUB_PERSONAL_ACCESS_TOKEN "https://api.github.com/repos/$owner/$repo/pulls" -d '{"title":"'$head'","body":"PR sent via github api","head":"'$head'","base":"'$base'"}'`
+number=`echo $pr | grep '^  "number":' | awk '{print $2}' | sed 's/,//'`
+curl -X PATCH -u $user:$token "https://api.github.com/repos/$owner/$repo/issues/$number" -d '{"assignees":["'$user'"]}'
+curl -X POST -u $user:$token "https://api.github.com/repos/$owner/$repo/pulls/$number" -d '{"reviewers":["akiratsai"]}'
