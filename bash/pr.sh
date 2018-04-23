a48822869a8c336b671a28e2d5029634571ba276
Qimat Luo <qimatluo@exosite.com>
2018-04-23 19:27:44 +0800

    Update pr body

--- bash/pr.sh
+++ bash/pr.sh
@@ -4,7 +4,7 @@ owner=`git remote -v | head -1 | cut -f2 | cut -d'/' -f1 | cut -d':' -f2`
 repo=`git remote -v | head -1 | cut -f2 | cut -d'/' -f2  | cut -d'.' -f1`
 base=$1
 head=`git rev-parse --abbrev-ref HEAD`
-pr=`curl -X POST -u $user:$token "https://api.github.com/repos/$owner/$repo/pulls" -d '{"title":"'$head'","body":"PR sent via github api","head":"'$head'","base":"'$base'"}'`
+pr=`curl -X POST -u $user:$token "https://api.github.com/repos/$owner/$repo/pulls" -d '{"title":"'$head'","body":"PR sent via https://github.com/QimatLuo/Setting/blob/master/bash/pr.sh","head":"'$head'","base":"'$base'"}'`
 echo $pr
 number=`echo $pr | awk -F '"number": ' '{print $2}' | cut -d',' -f1`
 curl -X PATCH -u $user:$token "https://api.github.com/repos/$owner/$repo/issues/$number" -d '{"assignees":["'$user'"]}'
