a08e3386b65592b96614ad7dad55267307528d61
Qimat Luo <qimatluo@exosite.com>
2018-04-25 16:34:55 +0800

    Default use develop branch as base

--- bash/pr.sh
+++ bash/pr.sh
@@ -3,6 +3,9 @@ user=`whoami`
 owner=`git remote -v | head -1 | cut -f2 | cut -d'/' -f1 | cut -d':' -f2`
 repo=`git remote -v | head -1 | cut -f2 | cut -d'/' -f2  | cut -d'.' -f1`
 base=$1
+if [ -z "$1" ]; then
+	base='develop'
+fi
 head=`git rev-parse --abbrev-ref HEAD`
 pr=`curl -X POST -u $user:$token "https://api.github.com/repos/$owner/$repo/pulls" -d '{"title":"'$head'","body":"PR sent via https://github.com/QimatLuo/Setting/blob/master/bash/pr.sh","head":"'$head'","base":"'$base'"}'`
 echo $pr
