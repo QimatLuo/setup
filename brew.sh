26cf8fc6617a2e2246ad93c83322e9114ea489f8
Qimat Luo <qimatluo@exosite.com>
2019-05-14 10:55:22 +0800

    fixup! fixup! wip

--- brew.sh
+++ brew.sh
@@ -1,16 +1,8 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# env
-brew install vim git bash-completion grep
-
-brew cask install docker slack keybase
+# general
+brew install vim git grep tmux node gradle
+brew cask install docker slack keybase steam google-chrome vlc
 
 # android
-brew tap caskroom/versions
-brew cask install java8
-mkdir ~/.android
-touch ~/.android/repositories.cfg
-brew cask install android-sdk
-sdkmanager "platform-tools"
-cd /usr/local/bin/
-ln -s /usr/local/Caskroom/android-sdk/4333796/platform-tools/adb .
+brew cask install adoptopenjdk8
